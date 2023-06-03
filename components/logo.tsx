88360e21a8774496b8ba8ba37181963dc5a0b42b
Bryan Gruneberg <hello@bryangruneberg.com>
2023-06-03 16:57:12 -0600

    Changed the logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,6 +3,6 @@ import Image from 'next/image'
 
 export function Logo({ ...props }) {
   return (
-    <Image src="/logo.png" width={247} height={193} alt="The composable umami logo" />
+    <Image src="/logo.png" width={491} height={111} alt="The composable umami logo" />
   )
 }
