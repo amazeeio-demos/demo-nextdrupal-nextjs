749ba7435b73cb5610d45afb7e98843dfa88ed50
Bryan Gruneberg <hello@bryangruneberg.com>
2023-06-03 14:32:10 -0600

    Make the AI

---
+++ pages/ai.tsx
@@ -0,0 +1,45 @@
+import { GetStaticPropsContext, GetStaticPropsResult } from "next"
+import { useTranslation } from "next-i18next"
+
+import { getGlobalElements } from "lib/get-global-elements"
+import { Breadcrumbs } from "components/breadcrumbs"
+import { Layout, LayoutProps } from "components/layout"
+import { PageHeader } from "components/page-header"
+import { FormContact } from "components/form--contact"
+
+interface ContactPageProps extends LayoutProps {}
+
+export default function ContactPage({ menus, blocks }: ContactPageProps) {
+  const { t } = useTranslation()
+  return (
+    <Layout
+      meta={{
+        title: t("website-ai"),
+      }}
+      menus={menus}
+      blocks={blocks}
+    >
+      <PageHeader
+        heading={t("website-ai")}
+        breadcrumbs={[
+          {
+            title: t("AI"),
+          },
+        ]}
+      />
+      <div className="container">
+	<iframe src="https://ai.demo.composetheweb.com/" className="aiframe" scrolling="no" />
+      </div>
+    </Layout>
+  )
+}
+
+export async function getStaticProps(
+  context: GetStaticPropsContext
+): Promise<GetStaticPropsResult<ContactPageProps>> {
+  return {
+    props: {
+      ...(await getGlobalElements(context)),
+    },
+  }
+}
